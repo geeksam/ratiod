51d2f88d813ff3b96950e1f2e6b549e7d03aeace
Sam Livingston-Gray <geeksam@gmail.com>
2020-05-27 18:07:55 -0700

    several more operations implemented

--- ratio.rb
+++ ratio.rb
@@ -3,12 +3,79 @@ class Ratio
   def initialize(numerator, denominator)
     raise ZeroDivisionError if denominator.zero?
 
-    @numerator   = numerator
-    @denominator = denominator
+    gcd = numerator.gcd(denominator)
+
+    @numerator   = numerator   / gcd
+    @denominator = denominator / gcd
+
+    if denominator < 0
+      @numerator *= -1
+      @denominator *= -1
+    end
+  end
+
+  def ==(other)
+    self.numerator == other.numerator && self.denominator == other.denominator
+  end
+
+  def +(other)
+    a1, b1 = self .numerator, self .denominator
+    a2, b2 = other.numerator, other.denominator
+    c1 = (a1 * b2) + (a2 * b1)
+    c2 = b1 * b2
+    self.class.new(c1, c2)
+  end
+
+  def -(other)
+    a1, b1 = self .numerator, self .denominator
+    a2, b2 = other.numerator, other.denominator
+    c1 = (a1 * b2) - (a2 * b1)
+    c2 = b1 * b2
+    self.class.new(c1, c2)
+  end
+
+  def *(other)
+    a1, b1 = self .numerator, self .denominator
+    a2, b2 = other.numerator, other.denominator
+    c1 = a1 * a2
+    c2 = b1 * b2
+    self.class.new(c1, c2)
+  end
+
+  def /(other)
+    a1, b1 = self .numerator, self .denominator
+    a2, b2 = other.numerator, other.denominator
+    c1 = a1 * b2
+    c2 = a2 * b1
+    self.class.new(c1, c2)
+  end
+
+  def **(power)
+    case power
+    when Integer
+      if power > 0
+        c1 = numerator ** power
+        c2 = denominator ** power
+      elsif power < 0
+        c1 = denominator ** power.abs
+        c2 = numerator ** power.abs
+      elsif power.zero?
+        c1, c2 = 1, 1 # by definition
+      end
+    end
+    self.class.new(c1, c2)
   end
 
   def to_s
     "%d/%d" % [ numerator, denominator ]
+  end
+
+  def inspect
+    "#<Ratio(%d, %d)>" % [ numerator, denominator ]
+  end
+
+  def abs
+    self.class.new( numerator.abs, denominator.abs )
   end
 end
 
--- ratio_spec.rb
+++ ratio_spec.rb
@@ -22,4 +22,77 @@ RSpec.describe Ratio do
     expect { Ratio(1, 0) }.to raise_error( ZeroDivisionError )
   end
 
+  specify "equality compares numerator and denominator" do
+    a = Ratio(1, 3)
+    b = Ratio(1, 3)
+    expect( a ).to eq( b )
+    expect( b ).to eq( a )
+
+    c = Ratio(2, 3)
+    expect( a ).to_not eq( c )
+    expect( b ).to_not eq( c )
+    expect( c ).to_not eq( a )
+    expect( c ).to_not eq( b )
+  end
+
+  specify "equality works no matter where the minus sign started out" do
+    a = Ratio(-1,  3)
+    b = Ratio( 1, -3)
+    expect( a ).to eq( b )
+    expect( b ).to eq( a )
+  end
+
+  specify "#abs (absolute value)" do
+    expect( Ratio( 1,  3).abs ).to eq( Ratio(1, 3) )
+    expect( Ratio(-1,  3).abs ).to eq( Ratio(1, 3) )
+    expect( Ratio( 1, -3).abs ).to eq( Ratio(1, 3) )
+    expect( Ratio(-1, -3).abs ).to eq( Ratio(1, 3) )
+  end
+
+  specify "reduction" do
+    r = Ratio(2, 4)
+    expect( r.numerator   ).to eq( 1 )
+    expect( r.denominator ).to eq( 2 )
+    expect( r.to_s        ).to eq( "1/2" )
+  end
+
+  specify "addition" do
+    expect( Ratio(1, 3) + Ratio(1, 3) ).to eq( Ratio(2, 3) )
+    expect( Ratio(1, 3) + Ratio(2, 3) ).to eq( Ratio(1, 1) )
+    expect( Ratio(1, 4) + Ratio(1, 2) ).to eq( Ratio(3, 4) )
+  end
+
+  specify "subtraction" do
+    expect( Ratio(1, 3) - Ratio(1, 3) ).to eq( Ratio(0, 3) )
+    expect( Ratio(1, 3) - Ratio(1, 6) ).to eq( Ratio(1, 6) )
+    expect( Ratio(1, 2) - Ratio(1, 4) ).to eq( Ratio(1, 4) )
+    expect( Ratio(1, 4) - Ratio(1, 2) ).to eq( Ratio(-1, 4) )
+  end
+
+  specify "multiplication" do
+    expect( Ratio(1, 2) * Ratio( 1,  2) ).to eq( Ratio( 1,  4) )
+    expect( Ratio(1, 3) * Ratio( 1,  6) ).to eq( Ratio( 1, 18) )
+    expect( Ratio(1, 2) * Ratio(-1,  2) ).to eq( Ratio(-1,  4) )
+    expect( Ratio(1, 2) * Ratio( 1, -2) ).to eq( Ratio(-1,  4) )
+  end
+
+  specify "division" do
+    expect( Ratio(1, 2) / Ratio( 1, 2) ).to eq( Ratio(1, 1) )
+    expect( Ratio(1, 3) / Ratio( 1, 6) ).to eq( Ratio(2, 1) )
+  end
+
+  specify "exponentiation to non-negative integer power" do
+    expect( Ratio(2, 3) ** 2 ).to eq( Ratio(4, 9) )
+    expect( Ratio(3, 5) ** 2 ).to eq( Ratio(9, 25) )
+
+    # for fun, let's just cross-check multiplication while we're here
+    expect( Ratio(2, 3) ** 2 ).to eq( Ratio(2, 3) * Ratio(2, 3) )
+    expect( Ratio(3, 5) ** 2 ).to eq( Ratio(3, 5) * Ratio(3, 5) )
+  end
+
+  specify "exponentiation to negative integer power" do
+    expect( Ratio(1, 3) ** -2 ).to eq( Ratio(9, 1) )
+    expect( Ratio(2, 3) ** -2 ).to eq( Ratio(9, 4) )
+    expect( Ratio(3, 5) ** -2 ).to eq( Ratio(25, 9) )
+  end
 end
